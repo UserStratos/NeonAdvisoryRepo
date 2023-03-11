edb6647b31e283430aed9f1e472dda2ce34f1fda
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-11 22:59:03 +0100

    import constants and functions from other directories

---
+++ run.py
@@ -0,0 +1,7 @@
+from ProjectStructure.pythonpackage1 import constants
+from ProjectStructure.pythonpackage2 import constants
+from ProjectStructure.pythonpackage1 import functions
+from ProjectStructure.pythonpackage2 import functions
+
+#Triggering the entire project
+#Do this by python run.py

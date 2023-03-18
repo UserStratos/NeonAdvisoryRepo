020e533c95cd1e974cb708de398f4d999e6ca8f7
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 15:08:42 +0100

    improving the date format such that it can be more readable

--- TimeSeriesModel/load_data/load_data.py
+++ TimeSeriesModel/load_data/load_data.py
@@ -12,16 +12,30 @@ import seaborn as sns
 data = pd.read_csv("numbers.csv")
 #print(data.to_string())
 
-print(data.info())
-print(data.head())
-print(data.tail())
-print(data.shape)
-print(data.describe())
-print(data.columns)
-print(data.dtypes)
+#print(data.info())
+#print(data.head())
+#print(data.tail())
+#print(data.shape)
+#print(data.describe())
+#print(data.columns)
+#print(data.dtypes)
+
+data['Year'] = data['Time Date'].apply(lambda x: str(x)[-4:])
+data['Month'] = data['Time Date'].apply(lambda x: str(x)[-6:-4])
+data['Day'] = data['Time Date'].apply(lambda x: str(x)[:-6])
+data['ds'] = pd.DatetimeIndex(data['Year']+'-'+data['Month']+'-'+data['Day'])
 
 
+new_data = data.loc[(data['Product']==2667437) & (data['Store']=='QLD_CW_ST0203')]
+new_data.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
+new_data.columns = ['y', 'ds']
 
+print(data)
+print(new_data)
+#Data cleaning / droping columns from teh data sample
+#new_data = data.drop(['Time Date'], axis=1)
+#print(new_data)
+#print(new_data.shape)
 
 
 

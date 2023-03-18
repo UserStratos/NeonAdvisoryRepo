e6187f1fdadf7baad88bb5cf2f0ad5b0be550850
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 13:21:34 +0100

    import and read at run.py the numbers.csv file

--- run.py
+++ run.py
@@ -1,23 +1,2 @@
-import warnings;
-warnings.simplefilter("ignore")
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-import csv
 
-
-#Load csv data file
-def load_data(filename):
-    mylist=[]
-    with open(filename) as numbers:
-        numbers_data=csv.reader(numbers, delimiter=",")
-        next(numbers_data)
-        for row in numbers_data:
-            mylist.append(row)
-        return mylist
-
-new_list = load_data('numbers.csv')
-for row in new_list:
-    print(row)
 

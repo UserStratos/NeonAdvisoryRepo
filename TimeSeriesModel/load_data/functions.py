86166b14afa8950891ce08596d02bb22265eae61
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 14:44:31 +0100

    Adding some basic functions to understand the data such as: data.info, data.head, data.tail, data.shape, data.describe,data.columns, data.dtypes

--- TimeSeriesModel/load_data/functions.py
+++ TimeSeriesModel/load_data/functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
 import numpy as np
-
+from TimeSeriesModel.load_data.load_data import load_series
 #Functions that are going to help calculations
+df.describe()
--- TimeSeriesModel/load_data/load_data.py
+++ TimeSeriesModel/load_data/load_data.py
@@ -4,23 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import csv
+#plt.style.use('ggplot')
+#pd.set_option('max_columns', 200)
+
+
+#Load data from csv file in to a dataframe
+data = pd.read_csv("numbers.csv")
+#print(data.to_string())
+
+print(data.info())
+print(data.head())
+print(data.tail())
+print(data.shape)
+print(data.describe())
+print(data.columns)
+print(data.dtypes)
 
 
 
-#Load csv data file
-def load_series(filename):
-    mylist=[]
-    with open(filename) as numbers:
-        numbers_data = csv.reader(numbers, delimiter=",")
-        next(numbers_data)
-        for row in numbers_data:
-            mylist.append(row)
-        return mylist
 
-new_list = load_series('numbers.csv')
-for row in new_list:
-    print(row)
+
 
 
 

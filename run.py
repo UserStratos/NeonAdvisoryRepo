0bdb30131557a04b33c5471e36beacdc42d2b7ec
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 11:19:37 +0100

    rename parts of the code structure

--- run.py
+++ run.py
@@ -1,36 +1,11 @@
-from ProjectStructure.pythonpackage1 import constants as c1
-from ProjectStructure.pythonpackage2 import constants as c2
-from ProjectStructure.pythonpackage1 import functions as h1
-from ProjectStructure.pythonpackage2 import functions as h2
-import pandas as pd
 import numpy as np
-import openpyxl
-
-from openpyxl import load_workbook
-
-book=load_workbook('numbers.xlsx')
-sheet=book.active
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
 
 
-rows=sheet.rows
+with open("./Book.csv", 'r') as file:
+  csvreader = csv.reader(file)
+  for row in csvreader:
+    print(row)
 
-headers=next(rows)
-
-headers=[cell.value for cell in next(rows)]
-print(headers)
-
-
-
-for row in rows:
-    data={}
-    for title, cell in zip(headers, row):
-        data[title] = cell.value
-    print(data)
-
-
-
-# Triggering the entire project
-# Do this by python run.py
-
-
-# enter a fictitious function for example

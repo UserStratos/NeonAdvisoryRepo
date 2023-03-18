efd525072aee024a44c2ef052a593cb7f80a8a95
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 12:27:49 +0100

    modify numbers.csv

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-
 df = pd.read_csv("numbers.csv")
+
 df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
 df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
 df['Day'] = df['Time Date'].apply(lambda x: str(x)[:-6])
@@ -14,4 +14,4 @@ df = df.loc[(df['Product']==2667437) & (df['Store']=='QLD_CW_ST0203')]
 df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
 df.columns = ['y', 'ds']
 
-df.hed()
+df.head()

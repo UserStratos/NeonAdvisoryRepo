1cf01e2366dc2cc75d956286236b76448ddf3fee
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 12:56:21 +0100

    import and read at run.py the numbers.csv file

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
-def load_data(filename):
+def load_csvdata(filename):
     mylist=[]
     with open(filename) as numbers:
         numbers_data=csv.reader(numbers, delimiter=",")
@@ -11,11 +11,11 @@ def load_data(filename):
             mylist.append(row)
         return mylist
 
-new_list = load_data('numbers.csv')
+new_list = load_csvdata('numbers.csv')
 for row in new_list:
     print(row)
 
-df = pd.read_csv("numbers.csv")
+#df = pd.read_csv("numbers.csv")
 
 #df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
 #df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
@@ -26,4 +26,4 @@ df = pd.read_csv("numbers.csv")
 #df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
 #df.columns = ['y', 'ds']
 
-df.head()
+#df.head()

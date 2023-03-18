e5510f13be6b8604f5a1450b2cc033f075a40f9c
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 12:11:27 +0100

    add some code on data loading at run.py

---
+++ TimeSeriesModel/Read&LoadData/Read&LoadData.py
@@ -0,0 +1,13 @@
+import warnings;
+warnings.simplefilter("ignore")
+import pandas as pd
+import numpy as np
+
+
+# Read Data
+
+df=pd.read_csv("numbers.csv")
+df.head()
+print("hello")
+
+
--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import csv
 
 
-with open("./Book.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  for row in csvreader:
-    print(row)
 
+df = pd.read_csv("numbers.csv")
+df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
+df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
+df['Day'] = df['Time Date'].apply(lambda x: str(x)[:-6])
+df['ds'] = pd.DatetimeIndex(df['Year']+'-'+df['Month']+'-'+df['Day'])
+
+df = df.loc[(df['Product']==2667437) & (df['Store']=='QLD_CW_ST0203')]
+df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
+df.columns = ['y', 'ds']
+
+df.hed()

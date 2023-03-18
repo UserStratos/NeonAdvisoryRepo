7256ec1caff6fab966c7220e24b5bf21be6d6799
UserStratos <127619367+UserStratos@users.noreply.github.com>
2023-03-18 13:18:31 +0100

    import and read at run.py the numbers.csv file

--- TimeSeriesModel/Read&LoadData/Read&LoadData.py
+++ TimeSeriesModel/Read&LoadData/Read&LoadData.py
@@ -1,13 +1,3 @@
-import warnings;
-warnings.simplefilter("ignore")
-import pandas as pd
-import numpy as np
 
-
-# Read Data
-
-df=pd.read_csv("numbers.csv")
-df.head()
-print("hello")
 
 
--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+import warnings;
+warnings.simplefilter("ignore")
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 import csv
-def load_csvdata(filename):
+
+
+#Load csv data file
+def load_data(filename):
     mylist=[]
     with open(filename) as numbers:
         numbers_data=csv.reader(numbers, delimiter=",")
@@ -11,19 +17,7 @@ def load_csvdata(filename):
             mylist.append(row)
         return mylist
 
-new_list = load_csvdata('numbers.csv')
+new_list = load_data('numbers.csv')
 for row in new_list:
     print(row)
 
-#df = pd.read_csv("numbers.csv")
-
-#df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
-#df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
-#df['Day'] = df['Time Date'].apply(lambda x: str(x)[:-6])
-#df['ds'] = pd.DatetimeIndex(df['Year']+'-'+df['Month']+'-'+df['Day'])
-
-#df = df.loc[(df['Product']==2667437) & (df['Store']=='QLD_CW_ST0203')]
-#df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
-#df.columns = ['y', 'ds']
-
-#df.head()
